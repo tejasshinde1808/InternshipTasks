eac68b3540803fb74ad8932b38268f834b2f4ab0
tejasshinde <shindetej2000@gmail.com>
2026-01-12 16:18:47 +0530

    Day 2

---
+++ Day 2/AssingmentOperator.java
@@ -0,0 +1,11 @@
+public class AssingmentOperator {
+    public static void main(String[] args) {
+        int a=10;
+        a+=5;
+        //a-=5;
+        a*=5;
+        a/=5;
+        System.out.println(a);
+
+    }
+}
---
+++ Day 2/If.java
@@ -0,0 +1,9 @@
+public class If {
+    public static void main(String args[]){
+        int age = 19;
+        if(age<18)
+            System.out.println("you are not adult");
+        else
+            System.out.println("you are adult");
+    }
+}
